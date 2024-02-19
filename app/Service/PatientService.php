dd7fea95d9fddf9713224eca332ef954a5d800dc
MOCHAMAD MUCHSIN ABDILLAH <70797619+muchsinabdillah@users.noreply.github.com>
2024-02-19 11:07:18 +0700

    x

--- app/Service/PatientService.php
+++ app/Service/PatientService.php
@@ -19,10 +19,105 @@ class PatientService extends Controller
     public function listksmgigi()
     {
         try {
-            return $this->GuzzleClientRequest(
-                env('API_URL_YARSI') . "registrations/getRegistrationRajalActiveCoas",
-                "GET" 
-            );
+            // return $this->GuzzleClientRequest(
+            //     env('API_URL_YARSI') . "registrations/getRegistrationRajalActiveCoas",
+            //     "GET" 
+            // );
+            $data['data'] = [
+                [
+                    "NoAntrianAll"=> "FE-2",
+                    "NamaJaminan"=>"BPJS Kesehatan",
+                    "PatientName"=>"FAKHRI FADHILLAH, TN",
+                    "Gander"=>"M",
+                    "Date_of_birth"=>"2009-12-15",
+                    "Address"=>"JL SUMUR BATU",
+                    "IdUnit"=>"46",
+                    "Visit_Date"=>"2024-02-16 14:07:17.000",
+                    "NamaUnit"=>"Poli Gigi Spesialis Orthodonty",
+                    "IdDokter"=>"3874",
+                    "NamaDokter"=>"drg. Fajar Eka Saputra, Sp.BM",
+                    "NoMR"=>"07-07-24",
+                    "NoEpisode"=>"OP070724-160224-0108",
+                    "NoRegistrasi"=>"RJJP160224-0493",
+                    "PatientType"=>"PERUSAHAAN",
+                    "StatusID"=>"0"
+                ],
+                [
+                    "NoAntrianAll"=> "FE-2",
+                    "NamaJaminan"=>"BPJS Kesehatan",
+                    "PatientName"=>"FAKHRI FADHILLAH, TN",
+                    "Gander"=>"M",
+                    "Date_of_birth"=>"2009-12-15",
+                    "Address"=>"JL SUMUR BATU",
+                    "IdUnit"=>"58",
+                    "Visit_Date"=>"2024-02-16 14:07:17.000",
+                    "NamaUnit"=>"Poli Gigi Spesialis Pedodonti",
+                    "IdDokter"=>"3874",
+                    "NamaDokter"=>"drg. Fajar Eka Saputra, Sp.BM",
+                    "NoMR"=>"07-07-24",
+                    "NoEpisode"=>"OP070724-160224-0108",
+                    "NoRegistrasi"=>"RJJP160224-0493",
+                    "PatientType"=>"PERUSAHAAN",
+                    "StatusID"=>"0"
+                ],
+                [
+                    "NoAntrianAll"=> "FE-2",
+                    "NamaJaminan"=>"BPJS Kesehatan",
+                    "PatientName"=>"FAKHRI FADHILLAH, TN",
+                    "Gander"=>"M",
+                    "Date_of_birth"=>"2009-12-15",
+                    "Address"=>"JL SUMUR BATU",
+                    "IdUnit"=>"59",
+                    "Visit_Date"=>"2024-02-16 14:07:17.000",
+                    "NamaUnit"=>"Poli Gigi Spesialis Periodonti",
+                    "IdDokter"=>"3874",
+                    "NamaDokter"=>"drg. Fajar Eka Saputra, Sp.BM",
+                    "NoMR"=>"07-07-24",
+                    "NoEpisode"=>"OP070724-160224-0108",
+                    "NoRegistrasi"=>"RJJP160224-0493",
+                    "PatientType"=>"PERUSAHAAN",
+                    "StatusID"=>"0"
+                ],
+                [
+                    "NoAntrianAll"=> "FE-2",
+                    "NamaJaminan"=>"BPJS Kesehatan",
+                    "PatientName"=>"FAKHRI FADHILLAH, TN",
+                    "Gander"=>"M",
+                    "Date_of_birth"=>"2009-12-15",
+                    "Address"=>"JL SUMUR BATU",
+                    "IdUnit"=>"60",
+                    "Visit_Date"=>"2024-02-16 14:07:17.000",
+                    "NamaUnit"=>"Poli Gigi Spesialis Prostodonti",
+                    "IdDokter"=>"3874",
+                    "NamaDokter"=>"drg. Fajar Eka Saputra, Sp.BM",
+                    "NoMR"=>"07-07-24",
+                    "NoEpisode"=>"OP070724-160224-0108",
+                    "NoRegistrasi"=>"RJJP160224-0493",
+                    "PatientType"=>"PERUSAHAAN",
+                    "StatusID"=>"0"
+                ],
+                [
+                    "NoAntrianAll"=> "FE-2",
+                    "NamaJaminan"=>"BPJS Kesehatan",
+                    "PatientName"=>"FAKHRI FADHILLAH, TN",
+                    "Gander"=>"M",
+                    "Date_of_birth"=>"2009-12-15",
+                    "Address"=>"JL SUMUR BATU",
+                    "IdUnit"=>"137",
+                    "Visit_Date"=>"2024-02-16 14:07:17.000",
+                    "NamaUnit"=>"Poli Gigi Spesialis Konservasi/Endodonsi",
+                    "IdDokter"=>"3874",
+                    "NamaDokter"=>"drg. Fajar Eka Saputra, Sp.BM",
+                    "NoMR"=>"07-07-24",
+                    "NoEpisode"=>"OP070724-160224-0108",
+                    "NoRegistrasi"=>"RJJP160224-0493",
+                    "PatientType"=>"PERUSAHAAN",
+                    "StatusID"=>"0"
+                ]
+                ]
+                ;
+
+           return $this->sendResponse($data, 'ditemukan !');
         } catch (\Exception $e) {
             throw new HttpException(200, $e);
         }
